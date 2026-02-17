e482bfe36381f9a68263bcab9b3e6f200dadfbf0
hammadai <hammadshakeel61@gmail.com>
2026-02-17 15:42:19 +0500

    Create my_script.py

---
+++ practise/my_script.py
@@ -0,0 +1,8 @@
+def my_method():
+    if __name__ == "__main__":
+        # Code here will only run if this script is executed directly
+        print("This script runs directly")
+    else:
+        print("This script runs indirectly")
+
+my_method()
